8c735238ceb2154baf8ec0e6cc209e810faa4a50
zainulabidin379 <ulabidinz1@gmail.com>
2022-09-25 18:05:25 +0500

    updated desktop screen

--- lib/desktop/desktop_home.dart
+++ lib/desktop/desktop_home.dart
@@ -3,6 +3,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 import '../../utils/constants.dart';
+import '../animations/entrance_fader.dart';
 import '../widgets/social_link.dart';
 
 class DesktopHomePage extends StatefulWidget {
@@ -27,106 +28,130 @@ class _DesktopHomePageState extends State<DesktopHomePage> {
             style: kAppBarText.copyWith(fontSize: 40),
           ),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              width: size.width,
-              height: size.height - 56,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: size.height - 56,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Opacity(
+                        opacity: 0.8,
+                        child: EntranceFader(
+                          offset: const Offset(0, 0),
+                          delay: const Duration(seconds: 1),
+                          duration: const Duration(milliseconds: 800),
+                          child: Image.asset(
+                            "assets/images/main4.png",
+                            fit: BoxFit.contain,
+                            height: size.width < 1200
+                                ? size.height * 0.8
+                                : size.height * 0.85,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 100, top: 200),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(
-                            "WELCOME TO MY PORTFOLIO",
-                            style: kDesktopBodyText.copyWith(
-                                fontSize: 22,
-                                color: kPrimary,
-                                fontFamily: "medium"),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                "WELCOME TO MY PORTFOLIO",
+                                style: kDesktopBodyText.copyWith(
+                                    fontSize: 22,
+                                    color: kPrimary,
+                                    fontFamily: "medium"),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              SizedBox(
+                                  height: 35,
+                                  width: 35,
+                                  child: Image.asset("assets/icons/hi.gif"))
+                            ],
                           ),
                           const SizedBox(
-                            width: 10,
+                            height: 25,
                           ),
-                          SizedBox(
-                              height: 35,
-                              width: 35,
-                              child: Image.asset("assets/icons/hi.gif"))
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 35,
-                      ),
-                      Text(
-                        "ZAIN UL ABIDIN",
-                        style: kDesktopBodyText.copyWith(
-                            fontSize: 70, fontFamily: "bold"),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.play_arrow_rounded,
-                            color: kPrimary,
-                            size: 25,
+                          RichText(
+                            text: TextSpan(
+                              text: 'ZAIN',
+                              style: kDesktopBodyText.copyWith(
+                                  fontSize: 90, fontFamily: "bold"),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: '\nUL ABIDIN',
+                                  style: kDesktopBodyText.copyWith(
+                                      fontSize: 90, fontFamily: "regular"),
+                                ),
+                              ],
+                            ),
                           ),
-                          AnimatedTextKit(
-                            animatedTexts: [
-                              TyperAnimatedText(
-                                ' Flutter Developer',
-                                speed: const Duration(milliseconds: 70),
-                                textStyle:
-                                    kDesktopBodyText.copyWith(fontSize: 22),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.play_arrow_rounded,
+                                color: kPrimary,
+                                size: 25,
                               ),
-                              TyperAnimatedText(
-                                ' UI/UX Enthusiast',
-                                speed: const Duration(milliseconds: 70),
-                                textStyle:
-                                    kDesktopBodyText.copyWith(fontSize: 22),
-                              ),
-                              TyperAnimatedText(
-                                ' A friend :)',
-                                speed: const Duration(milliseconds: 70),
-                                textStyle:
-                                    kDesktopBodyText.copyWith(fontSize: 22),
+                              AnimatedTextKit(
+                                animatedTexts: [
+                                  TyperAnimatedText(
+                                    ' Flutter Developer',
+                                    speed: const Duration(milliseconds: 70),
+                                    textStyle:
+                                        kDesktopBodyText.copyWith(fontSize: 22),
+                                  ),
+                                  TyperAnimatedText(
+                                    ' UI/UX Enthusiast',
+                                    speed: const Duration(milliseconds: 70),
+                                    textStyle:
+                                        kDesktopBodyText.copyWith(fontSize: 22),
+                                  ),
+                                  TyperAnimatedText(
+                                    ' A friend :)',
+                                    speed: const Duration(milliseconds: 70),
+                                    textStyle:
+                                        kDesktopBodyText.copyWith(fontSize: 22),
+                                  ),
+                                ],
+                                isRepeatingAnimation: true,
+                                repeatForever: true,
                               ),
                             ],
-                            isRepeatingAnimation: true,
-                            repeatForever: true,
+                          ),
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          Row(
+                            children: const [
+                              SocialLink(icon: "facebook"),
+                              SocialLink(icon: "instagram"),
+                              SocialLink(icon: "linkedin"),
+                              SocialLink(icon: "github"),
+                              SocialLink(icon: "fiverr"),
+                              SocialLink(icon: "upwork"),
+                            ],
                           ),
                         ],
                       ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      Row(
-                        children: const [
-                          SocialLink(icon: "facebook"),
-                          SocialLink(icon: "instagram"),
-                          SocialLink(icon: "linkedin"),
-                          SocialLink(icon: "github"),
-                          SocialLink(icon: "fiverr"),
-                          SocialLink(icon: "upwork"),
-                        ],
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: size.height * 0.6,
-                    child: Opacity(
-                        opacity: 0.9,
-                        child: Image.asset("assets/images/main.png")),
-                  )
-                ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
